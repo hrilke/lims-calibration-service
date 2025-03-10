004d334b25c9a50157b6c0dd3c5de2780d614909
Hrishikesh Shelke <dev.hrishi@outlook.com>
2025-03-10 20:36:37 +0530

    Initial commit for lims-calibration-service

---
+++ src/main/java/com/capstone/lims/calibration_service/CalibrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.lims.calibration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalibrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalibrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/lims/calibration_service/CalibrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.lims.calibration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalibrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
